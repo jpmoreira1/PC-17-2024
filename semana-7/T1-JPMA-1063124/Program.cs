78b91e7bcb7873931a3b483e4fa23fd3c96409f7
jpmoreira1 <157499468+jpmoreira1@users.noreply.github.com>
2024-02-27 09:12:28 -0600

    Add files via upload

---
+++ semana-7/T1-JPMA-1063124/Program.cs
@@ -0,0 +1,19 @@
+﻿string nombre = "";
+string edad = "";
+string carrera = "";
+string carne = "";
+
+Console.WriteLine("Ingrese su nombre");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese su edad");
+edad = Console.ReadLine();
+
+Console.WriteLine("Ingrese su carrera");
+carrera = Console.ReadLine();
+
+Console.WriteLine("Ingrese su carné");
+carne = Console.ReadLine();
+
+Console.WriteLine($"Soy {nombre}, tengo {edad} años y estudio la carrera de {carrera}. \nMi número de carné es {carne}");
+Console.ReadLine();
