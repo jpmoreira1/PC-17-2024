8ae0daa9ee15b3b7d4725b9e1eb389b7c2d390a8
jpmoreira1 <157499468+jpmoreira1@users.noreply.github.com>
2024-02-27 09:49:50 -0600

    Add files via upload

---
+++ semana-7/T2-JPMA-1063124-E1/Program.cs
@@ -0,0 +1,15 @@
+﻿double v0;
+double aceleracion; 
+double tiempo;  
+
+Console.WriteLine("Ingrese la velocidad inicial");
+v0 = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la aceleración");
+aceleracion = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el tiempo");
+tiempo = Double.Parse(Console.ReadLine());
+
+Console.WriteLine($"La velocidad final es {v0 + aceleracion * tiempo}");
+
