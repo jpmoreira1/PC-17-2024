ef143409b8da1aa87befb7eb44790f3d4193c6cb
jpmoreira1 <157499468+jpmoreira1@users.noreply.github.com>
2024-03-12 09:48:55 -0600

    Add files via upload

---
+++ semana-9/fibonacci/L4-JPMA-1063124/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+Pablo Moreira
+1063124
+Fibonacci
+*/
+
+
+int n = 0;
+int a = 0;
+int b = 1;
+int c = 0;
+int i = 2;
+string resultado = "";
+
+Console.WriteLine("Ingresa un numero");
+n = int.Parse(Console.ReadLine());
+
+resultado += a + "";
+resultado += b + "";
+
+while (i < n)
+{
+    c = a + b;
+    a = b;
+    b = c;
+    resultado += c + "";
+    i++;
+}
+
+Console.WriteLine(resultado);
